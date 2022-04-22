0c65607213091fbc59bb7d2fc945020326553c88
mika <mika.marcondes2@gmail.com>
2022-04-22 20:59:46 -0300

    wip: added toggle on/off for the mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function toggleMenu() {
 
-    let menu = document.getElementById("ham-container")
-    let width = window.innerWidth.toString()
+    let menu = document.getElementById('mobileMenu')
+    let item = menu.classList.item(0).toString()
 
-    if (menu.style.display === 'none') {
-        menu.style.display = 'flex'
-    } else {
-        menu.style.display = 'none'
+    if (item === 'hide') {
+        showMenu()
     }
 
-}
+    if (item === 'show') {
+        hideMenu()
+    }
 
+    function showMenu() {
+        menu.classList.add('show')
+        menu.classList.remove('hide')
+    }
+
+    function hideMenu() {
+        menu.classList.remove('show')
+        menu.classList.add('hide')
+    }
+}
